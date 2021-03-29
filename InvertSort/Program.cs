d8b26310771867ebbc8548e24e8398e490ad21a6
NurMan <elnurmaniac@gmail.com>
2021-03-29 21:06:24 +0300

    @shawdown исправил пробелы, доабвил комментарии к действиям метода, создал новый массив, дабы не менять старый.

--- InvertSort/Program.cs
+++ InvertSort/Program.cs
@@ -10,26 +10,30 @@ namespace InvertSort
     {
         static void Main(string[] args)
         {
-            int[,] nums = { { 1,2,3,4,5}, { 6,7,8,9,10}, { 11,12,13,14,15 }, { 16,17,18,19,20 },{ 21,22,23,24,25} };
+            int[,] nums = { { 1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}, {16,17,18,19,20}, {21,22,23,24,25} };
             int[,] invertNums = Invert(nums);
             int rows = invertNums.GetUpperBound(0) + 1;
             int columns = invertNums.Length / rows;
+
             for (int a = 0; a < rows; a++)
             {
-
                 for (int b = 0; b < columns; b++)
                 {
-                    Console.Write($"{nums[a, b]} \t");
+                    Console.Write($"{invertNums[a, b]} \t");
                 }
                 Console.WriteLine();
             }
             Console.ReadKey();
         }
-        static int[,] Invert(int[,] invertNums)
+        /// <summary>
+        /// Метод возвращает новый массив, с инвертированным порядком элементов заложенного массива
+        /// </summary>
+        /// <param name="nums">массив</param>
+        /// <returns> инвертированный массив</returns>
+        static int[,] Invert(int[,] nums)
         {
-
-            int rows = invertNums.GetUpperBound(0) + 1;
-            int columns = invertNums.GetUpperBound(1) + 1;
+            int rows = nums.GetUpperBound(0) + 1;
+            int columns = nums.GetUpperBound(1) + 1;
 
             if (rows != columns)
             {
@@ -37,7 +41,15 @@ namespace InvertSort
                 Console.ReadKey();
                 Environment.Exit(0);
             }
+            int[,] invertNums = new int [rows, columns] ;
 
+            for (int s = 0; s < rows; ++s)
+            {
+                for (int d = 0; d < columns; ++d)
+                { 
+                invertNums [s,d]  = nums [s,d] ;
+                }
+            }
             int swap = 0; // переменная будет заменять элементы в массиве
             int numberOfElementInRow = 0; // переменная указывает номер элемента в ряду
 
