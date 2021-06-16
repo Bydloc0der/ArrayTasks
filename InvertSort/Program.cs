5cbcfed5d27a5b0751410ebea6d0365828607fc5
NurMan <elnurmaniac@gmail.com>
2021-06-16 16:42:27 +0300

    @shawdown В InvertSort исправил отступы. В SpiralArray косяк. Если задать числу N значения 9,11,13 - то всё ок, спирали рисуются правильно. Если задать 15 - появляются отступы гигантские, но спираль рисуется верно. Если от 17 и больше - получается биллиберда.

--- InvertSort/Program.cs
+++ InvertSort/Program.cs
@@ -10,7 +10,7 @@ namespace InvertSort
     {
         static void Main(string[] args)
         {
-            int[,] nums = { { 1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}, {16,17,18,19,20}, {21,22,23,24,25} };
+            int[,] nums = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 }, { 16, 17, 18, 19, 20 }, { 21, 22, 23, 24, 25 } };
             int[,] invertNums = Invert(nums);
             int rows = invertNums.GetUpperBound(0) + 1;
             int columns = invertNums.Length / rows;
@@ -25,11 +25,12 @@ namespace InvertSort
             }
             Console.ReadKey();
         }
+
         /// <summary>
         /// Метод возвращает новый массив, с инвертированным порядком элементов заложенного массива
         /// </summary>
         /// <param name="nums">массив</param>
-        /// <returns> инвертированный массив</returns>
+        /// <returns> Инвертированный массив</returns>
         static int[,] Invert(int[,] nums)
         {
             int rows = nums.GetUpperBound(0) + 1;
@@ -41,27 +42,29 @@ namespace InvertSort
                 Console.ReadKey();
                 Environment.Exit(0);
             }
-            int[,] invertNums = new int [rows, columns] ;
+
+            int[,] invertNums = new int[rows, columns];
 
             for (int s = 0; s < rows; ++s)
             {
                 for (int d = 0; d < columns; ++d)
-                { 
-                invertNums [s,d]  = nums [s,d] ;
+                {
+                    invertNums[s, d] = nums[s, d];
                 }
             }
+
             int swap = 0; // переменная будет заменять элементы в массиве
             int numberOfElementInRow = 0; // переменная указывает номер элемента в ряду
 
-            for (int a = rows-1; a > 0; --a)
+            for (int a = rows - 1; a > 0; --a)
             {
                 int counter = 1;// переменная позволяет отсчитывать расстояние между элементами, которые будут меняться, при помощи переменной "swap"
-                
+
                 while (a - counter >= 0)
                 {
                     swap = invertNums[a - counter, numberOfElementInRow];
-                    invertNums[a - counter, numberOfElementInRow] = invertNums[a , numberOfElementInRow + counter];
-                    invertNums[a , numberOfElementInRow + counter] = swap;
+                    invertNums[a - counter, numberOfElementInRow] = invertNums[a, numberOfElementInRow + counter];
+                    invertNums[a, numberOfElementInRow + counter] = swap;
                     ++counter;
                 }
 
---
+++ SpiralArray/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiralArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 13;
+            char X = 'X'; 
+            char space = ' ';
+            char[,] array = MadeSpiral(N,X,space);
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+            for (int c = 0; c < rows; c++)
+            {
+                for (int d = 0; d < columns; d++)
+                {
+                    Console.Write($"{array[c, d]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Метод возвращает двухмерный массив символов, выстраивающих сприраль.
+        /// </summary>
+        /// <param name="N">Число, обозначающее размер массива</param>
+        /// <param name="X">Символ, который будет "стенкой" спирали</param>
+        /// <param name="space"> Символ, который будет "пустотой" в спирали</param>
+        /// <returns></returns>
+
+        static char[,] MadeSpiral (int N, char X, char space)
+        {
+            if (0 == N % 2 || N < 5)
+            {
+                Console.WriteLine("Ошибка. Аргумент N не может быть меньше 5 и чётным.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            char[,] array = new char[N, N];
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+            int s = N - 1;
+
+            for (int a = 0; a < N+1/2; ++a)
+            {
+                char mark;
+                if (a % 2 == 0)
+                {
+                    mark = X;
+                }
+                else
+                {
+                    mark = space;
+                }
+                    
+                int counter = 0;
+
+                while (counter + a < N - a)
+                {
+                    array[a, a + counter] = mark;
+                    array[a + counter, a] = mark;
+
+                    array[s, s - counter] = mark;
+                    array[s - counter, s] = mark;
+
+                    ++counter;
+                }
+                --s;
+            }
+
+            for (int c = 0; c < N / 2; ++c)
+            {
+                if (array[c + 1, c] == X)
+                {
+                    array[c + 1, c] = space;
+                }
+                else
+                {
+                    array[c + 1, c] = X;
+                }
+            }
+
+            return array;
+        }
+    }
+}
