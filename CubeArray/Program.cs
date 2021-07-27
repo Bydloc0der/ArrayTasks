8c1042edfbba86ccd84c049266aaca8aff7faf30
NurMan <elnurmaniac@gmail.com>
2021-07-27 20:12:29 +0300

    @shawdown, добавил символ space, отвечающий за пустое пространггство в кубе, создал новый метод, для вывода на экран результата., поменял вывод на экран (теперь. если ввести не число и не "выход", программа сообщит об ошибке и не закроется, пока не будут введены верные данные)

--- CubeArray/Program.cs
+++ CubeArray/Program.cs
@@ -11,16 +11,106 @@ namespace CubeArray
         static void Main(string[] args)
         {
             Console.WriteLine("Введите целое число, не меньше нуля. Чтобы выйти из программы - напишите 'выход'.");
-            string result = Console.ReadLine();// результат ввода пользователем.
-
-            if (result == "выход")
+            for (int f = 0; f < 1;)
             {
+                string result = Console.ReadLine();// результат ввода пользователем.
+                int cubeSize; // размер куба
+                bool checkNumber = int.TryParse(result, out cubeSize);
+
+                if (result == "выход")
+                {
+                    Environment.Exit(0);
+                }
+                else if (checkNumber == true)
+                {
+                    char[][][] nums = GetTheCube(cubeSize);
+                    GetResultOnScreen(nums, cubeSize);
+                    ++f;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка. Введены не верные данные. Введите целое число, не меньше нуля. Чтобы выйти из программы - напишите 'выход'.  ");
+                }
+            }
+        }
+        /// <summary>
+        /// Метод возвращает заполненный массив, символом X
+        /// </summary>
+        /// <param name="cubeSize"></param>
+        /// <returns>Заполненный зубчатый массив</returns>
+        static char[][][] GetTheCube(int cubeSize)
+        {
+
+            if (cubeSize < 1)
+            {
+                Console.WriteLine("Ошибка. Введены не верные данные");
+                Console.ReadKey();
                 Environment.Exit(0);
             }
 
-            int cubeSize = Convert.ToInt32(result);// переменная означает размер сторон куба 
-            char[][][] nums = GetTheCube(cubeSize);
+            char symbol = 'X';// ссимвол, отображающий грани куба
+            char space = ' ';// символ, отвечающий за пустое пространство, при вывроде на экран
+            char[][][] nums = new char[cubeSize][][];
 
+            for (int a = 0; a < cubeSize; ++a)
+            {
+                nums[a] = new char[cubeSize][];
+
+                if (a == 0 || a == nums[a].Length - 1)
+                {
+                    for (int b = 0; b < nums[a].Length; ++b)
+                    {
+                        nums[a][b] = new char[cubeSize];
+
+                        for (int c = 0; c < nums[a][b].Length; ++c)
+                        {
+
+                            if (b == 0 || b == nums[a][b].Length - 1)
+                            {
+                                nums[a][b][c] = symbol;
+                            }
+                            else if (c == 0 || c == nums[a][b].Length - 1)
+                            {
+                                nums[a][b][c] = symbol;
+                            }
+                            else
+                            {
+                                nums[a][b][c] = space;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    for (int d = 0; d < cubeSize; ++d)
+                    {
+                        nums[a][d] = new char[cubeSize];
+                        for (int e = 0; e < cubeSize; ++e)
+                        {
+                            if ((d == 0 || d == nums[a][d].Length - 1) && (e == 0 || e == nums[a][d].Length - 1))
+                            {
+                                nums[a][d][d] = symbol;
+                                nums[a][0][d] = symbol;
+                                nums[a][d][0] = symbol;
+                            }
+                            else
+                            {
+                                nums[a][d][e] = space;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return nums;
+        }
+        /// <summary>
+        /// Метод вывдоит результат на экран (срез куба)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="cubeSize"></param>
+        static void GetResultOnScreen(char[][][] nums, int cubeSize)
+        {
             for (int i = 0; i < cubeSize; ++i)
             {
                 Console.WriteLine($"Срез {i + 1}");
@@ -41,73 +131,6 @@ namespace CubeArray
 
             Console.ReadKey();
 
-        }
-        /// <summary>
-        /// Метод возвращает заполненный массив, символом X
-        /// </summary>
-        /// <param name="cubeSize"></param>
-        /// <returns>Заполненный зубчатый массив</returns>
-        static char[][][] GetTheCube(int cubeSize)
-        {
-
-            if (cubeSize < 0)
-            {
-                Console.WriteLine("Ошибка. Неверно введён аргумент.");
-                Console.ReadKey();
-                Environment.Exit(0);
-            }
-
-            char symbol = 'X';
-            char[][][] nums = new char[cubeSize][][];
-
-            for (int a = 0; a < cubeSize; ++a)
-            {
-                nums[a] = new char[cubeSize][];
-
-                if (a == 0 || a == nums[a].Length - 1)
-                {
-                    for (int b = 0; b < nums[a].Length; ++b)
-                    {
-                        nums[a][b] = new char[cubeSize];
-
-                        for (int c = 0; c < nums[a][b].Length; ++c)
-                        {
-
-                            if (b == 0 || b == nums[a][b].Length - 1)
-                            {
-                                nums[a][b][c] = symbol;
-                            }
-                            else
-                            {
-                                if (c == 0 || c == nums[a][b].Length - 1)
-                                {
-                                    nums[a][b][c] = symbol;
-                                }
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    for (int d = 0; d < cubeSize; ++d)
-                    {
-                        nums[a][d] = new char[cubeSize];
-
-                        if (d == 0 || d == nums[a][d].Length - 1)
-                        {
-                            nums[a][d][d] = symbol;
-                            nums[a][0][d] = symbol;
-                            nums[a][d][0] = symbol;
-                        }
-                        else
-                        {
-                            continue;
-                        }
-                    }
-                }
-            }
-
-            return nums;
         }
     }
 }
